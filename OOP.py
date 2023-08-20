efc6f92c490df70aa75bd77ed8700c734450b5f3
DeolaRichard <adekunlerichie5@gmail.com>
2023-08-20 15:30:55 +0100

    Learning to create multiple classes

--- OOP.py
+++ OOP.py
@@ -27,5 +27,30 @@ print(d.get_name(), "is ", d.get_age(), " years old")
 d2 = Dog("Biden", 13)
 d2.set_age(65)
 print(d2.get_name(), "is ", d2.get_age(), " years old")
+
+
 # d.bark()
 # print(d.add_one(5))
+
+# Learning to create Multiple Classes
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade  # 0 - 100
+
+    def get_grade(self):  # Method that returns  the students grade
+        return self.grade
+
+
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) <= self.max_students:
+            self.students.append(student)
+
