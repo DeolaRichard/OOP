760b2f583fbfecf81a741e808b6fd6690620a89b
DeolaRichard <adekunlerichie5@gmail.com>
2023-08-20 14:37:26 +0100

    Modifying attributes using methods

--- OOP.py
+++ OOP.py
@@ -12,6 +12,8 @@ class Dog:
     def get_age(self):
         return self.age
 
+    def set_age(self, age):  # Modifying attributes
+        self.age = age
     # def add_one(self, x):
     #     return x + 1
     # def bark(self):
@@ -20,8 +22,10 @@ class Dog:
 
 # creating a new instance of the class dog
 d = Dog("Merlin", 12)
+d.set_age(52)
 print(d.get_name(), "is ", d.get_age(), " years old")
 d2 = Dog("Biden", 13)
+d2.set_age(65)
 print(d2.get_name(), "is ", d2.get_age(), " years old")
 # d.bark()
 # print(d.add_one(5))
